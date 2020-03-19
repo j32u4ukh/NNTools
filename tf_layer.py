d9364e4621f5b3756c211fc29ffd2bee1b74591b
j32u4ukh <j32u4ukh@gmail.com>
2020-03-19 09:33:45 +0800

    discriminate tensorflow and pytorch

--- tf_layer.py
+++ tf_layer.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 def addLayer(inputs, out_size, activation=None):
     Weights = tf.Variable(tf.random_normal([int(inputs.shape[1]), out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-
     Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
+
     if activation is None:
         outputs = Wx_plus_b
     else:
